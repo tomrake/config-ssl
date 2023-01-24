149d6b8885e78e9089d9ec15584644bc7cb7d7ff
Tom Rake <zzzap1957@gmail.com>
2023-01-23 20:42:34 -0500

    Works by image changes.

--- main.lisp
+++ main.lisp
@@ -9,5 +9,4 @@
 
 (defun preload ()
  (cl+ssl/config:define-libcrypto-path "c:/devel/msys64/mingw64/bin/libcrypto-1_1-x64.dll")
- (cl+ssl/config:define-libssl-path "c:/devel/msys64/mingw64/bin/libssl-1_1-x64.dll") 
-  (ql:quickload "cl+ssl"))
+ (cl+ssl/config:define-libssl-path "c:/devel/msys64/mingw64/bin/libssl-1_1-x64.dll"))
