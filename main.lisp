47fd2795097f768ba23e6a04817e341af4b1c46d
Tom Rake <zzzap1957@gmail.com>
2024-05-01 20:51:43 -0400

    WIP

--- main.lisp
+++ main.lisp
@@ -2,13 +2,12 @@
   (:use
    #:cl
    #:cl+ssl/config)
-  (:export
-   #:preload))
+  (:export))
 
 (in-package #:config-ssl/main)
 
-(defun preload ()
- (cl+ssl/config:define-libcrypto-path "c:/devel/msys64/mingw64/bin/libcrypto-1_1-x64.dll")
- (cl+ssl/config:define-libssl-path "c:/devel/msys64/mingw64/bin/libssl-1_1-x64.dll"))
 
-(preload)
+
+
+(cl+ssl/config:define-libcrypto-path "c:/devel/msys64/ucrt64/bin/libcrypto-3-x64.dll")
+(cl+ssl/config:define-libssl-path "c:/devel/msys64/ucrt64/bin/libssl-3-x64.dll")
